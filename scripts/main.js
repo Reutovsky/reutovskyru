7aa8f1b5ca37b1d177def78f8ead2bc97ce797d9
Dmitry Ivanov <Webdev.Dmitry@gmail.com>
2018-07-04 21:29:43 +0300

    welcome page

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    Particles.init({
+      selector: '.background',
+      speed: 1,
+      maxParticles: 50,
+      sizeVariations: 1,
+      minDistance: 100,
+      connectParticles: true
+    });
+  };
